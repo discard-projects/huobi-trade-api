f6a786e8130c806600aa10f855745d40e72efa88
sen <578595193@qq.com>
2019-07-02 17:12:19 +0800

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  config.active_job.queue_adapter     = :sidekiq
+  # config.active_job.queue_adapter     = :sidekiq
   # config.active_job.queue_name_prefix = "huobi_auto_trade_development"
 
 
