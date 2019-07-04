64223b186c85c54ac8fd2d10a4f8d1718fb04c3f
sen <578595193@qq.com>
2019-07-04 11:24:21 +0800

    fix

--- app/controllers/api/v1/trade_symbols_controller.rb
+++ app/controllers/api/v1/trade_symbols_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::TradeSymbolsController < Api::V1::BaseController
   end
 
   def toggle_switch
-    trade_symbol = TradeSymbol.find(id: params[:id])
+    trade_symbol = TradeSymbol.find(params[:id])
     # 如果目前是开启状态要关闭，但是已经有纳入的用户，拒绝关闭
     if trade_symbol.enabled && trade_symbol.users.present?
       render json: {msg: 'invalid! can not switch'}, status: 422 and return
