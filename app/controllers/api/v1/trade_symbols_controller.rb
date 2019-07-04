27ae128a16c36453271f3878b49b54e75b3321e2
sen <578595193@qq.com>
2019-07-04 11:17:01 +0800

    fix

--- app/controllers/api/v1/trade_symbols_controller.rb
+++ app/controllers/api/v1/trade_symbols_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::TradeSymbolsController < Api::V1::BaseController
   # skip_before_action :authenticate_user!, only: [:index]
   def index
-    @is_manager = current_user.is_manager?
     super TradeSymbol.order(enabled: :desc)
   end
 
--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::UserController < Api::V1::BaseController
   skip_before_action :authenticate_user!, only: [:create]
 
   def create
-    super
+    super do |is_success|
+      AccountsFetchJob.perform_later() if is_success
+    end
   end
 end
