7190a9b61c84f373f6ce5d58ef8aa9f0cf59b5e2
sen <578595193@qq.com>
2019-07-04 10:02:23 +0800

    api toggle switch

--- app/controllers/api/v1/trade_symbols_controller.rb
+++ app/controllers/api/v1/trade_symbols_controller.rb
@@ -6,7 +6,11 @@ class Api::V1::TradeSymbolsController < Api::V1::BaseController
   end
 
   def toggle_switch
-    if current_user.is_manager?
+    trade_symbol = TradeSymbol.find(id: params[:id])
+    # 如果目前是开启状态要关闭，但是已经有纳入的用户，拒绝关闭
+    if trade_symbol.enabled && trade_symbol.users.present?
+      render json: {msg: 'invalid! can not switch'}, status: 422 and return
+    else
       super
     end
   end
