ca75a16186403205b5a16827eb349f3b2c23c115
sen <578595193@qq.com>
2019-07-17 17:43:42 +0800

    FIX: Interval 下单失败重新，下单

--- app/jobs/balance_intervals_each_job.rb
+++ app/jobs/balance_intervals_each_job.rb
@@ -12,7 +12,15 @@ class BalanceIntervalsEachJob < ApplicationJob
         if huobi_api
           buy_price = [trade_symbol.current_price, balance_interval.buy_price].min.floor(trade_symbol.price_precision)
           order_interval = balance_interval.order_intervals.create(price: buy_price, amount: balance_interval.amount, category: 'category_buy')
-          order_interval.status_trading! if order_interval.may_status_trading?
+          if order_interval.may_status_trading?
+            order_interval.status_trading! rescue nil
+          end
+        end
+      elsif balance_interval.order_intervals.where(status: [:status_trading, :status_traded]).blank?
+        balance_interval.order_intervals.category_buy.status_created.each do |order_interval|
+          if order_interval.may_status_trading?
+            order_interval.status_trading! rescue nil
+          end
         end
       end
     end
