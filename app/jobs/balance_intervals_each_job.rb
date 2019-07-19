a7caa6e74b9b7570b586d296cbb10546d31d650c
sen <578595193@qq.com>
2019-07-19 09:30:45 +0800

    fix

--- app/jobs/balance_intervals_each_job.rb
+++ app/jobs/balance_intervals_each_job.rb
@@ -10,7 +10,8 @@ class BalanceIntervalsEachJob < ApplicationJob
         huobi_api = user.huobi_api
         trade_symbol = balance_interval.trade_symbol
         if huobi_api
-          buy_price = [trade_symbol.current_price, balance_interval.buy_price].min.floor(trade_symbol.price_precision)
+          # buy_price = [trade_symbol.current_price, balance_interval.buy_price].min.floor(trade_symbol.price_precision)
+          buy_price = balance_interval.buy_price.floor(trade_symbol.price_precision)
           order_interval = balance_interval.order_intervals.create(price: buy_price, amount: balance_interval.amount, category: 'category_buy')
           if order_interval.may_status_trading?
             order_interval.status_trading! rescue nil
