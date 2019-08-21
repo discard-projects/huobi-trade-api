9c8ea4558405b7995c51dc5fd32afb22ad4de61e
sen <578595193@qq.com>
2019-08-21 11:39:04 +0800

    .

--- app/jobs/balances_job.rb
+++ app/jobs/balances_job.rb
@@ -3,18 +3,26 @@ class BalancesJob < ApplicationJob
 
   def perform(*args)
     # Do something later
-    # balance_intervals
-    BalanceInterval.where(enabled: true).each do |balance_interval|
-      BalanceIntervalsEachJob.perform_later(balance_interval.id)
+
+    Rails.cache.fetch("BalancesJob:Intervals:Plans", expires_in: 1.seconds) do
+      # balance_intervals
+      BalanceInterval.where(enabled: true).each do |balance_interval|
+        BalanceIntervalsEachJob.perform_later(balance_interval.id)
+      end
+      # balance_plans
+      BalancePlan.where(enabled: true).each do |balance_plan|
+        BalancePlansEachJob.perform_later(balance_plan.id)
+      end
     end
-    # balance_smarts
-    BalanceSmart.where(enabled: true).each do |balance_smart|
-      BalanceSmartsEachJob.perform_later(balance_smart.id)
+
+    Rails.cache.fetch("BalancesJob:Smarts", expires_in: 30.seconds) do
+      # balance_smarts
+      BalanceSmart.where(enabled: true).each do |balance_smart|
+        BalanceSmartsEachJob.perform_later(balance_smart.id)
+      end
     end
-    # balance_plans
-    BalancePlan.where(enabled: true).each do |balance_plan|
-      BalancePlansEachJob.perform_later(balance_plan.id)
-    end
-    BalancesJob.set(wait: 5.second).perform_later()
+
+    # BalancesJob.set(wait: 5.second).perform_later()
+    BalancesJob.perform_later()
   end
 end
