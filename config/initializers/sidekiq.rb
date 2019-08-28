1b07e1025b4061b1689997f9abebf7436c7aa57f
sen <578595193@qq.com>
2019-08-28 13:02:11 +0800

    FIX: logger level

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
+Sidekiq::Logging.logger.level = Logger::WARN
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://127.0.0.1:6379/6' }
   config.error_handlers << Proc.new do |ex, ctx_hash|
-    p '..........', ex, ctx_hash
     # $slack_bug_notifier.ping "ex: #{ex}, ctx_hash: #{ctx_hash}"
   end
 end
